1ed3407984b72bfdf8dc814665ce0d482a69001e
Ashside <maybeyushu@outlook.com>
2024-01-11 23:53:55 +0800

    完成求值部分

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -294,3 +294,17 @@ func TestFunctionApplication(t *testing.T) {
 
 	}
 }
+
+func TestClosure(t *testing.T) {
+
+	input := `
+	let newAdder=fn(x){
+		fn(y){x+y;};
+	};
+	let addTwo=newAdder(2);
+	addTwo(2);
+	`
+
+	testIntegerObject(t, testEval(input), 4)
+
+}
